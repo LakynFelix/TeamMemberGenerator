af66d1e3d696471389741ee4ca838359f85c7a36
Lakyn <lakynmphotography@hotmail.com>
2021-07-28 02:21:55 -0400

    changes

--- __test__/Employee.test.js
+++ __test__/Employee.test.js
@@ -1,1 +1,10 @@
-test
+const Employee = require('../lib/Employee')
+
+test('creates a new Employee'), () => {
+    const Employee(new Employee('Lakyn', 'Employee', 'lakynmphotography@hotmail.com', '135634'));
+
+expect(Employee.name).toBe('Lakyn');
+expect(Employee.role).toBe('Employee');
+expect(Employee.email).toBe('lakynmphotography@hotmail.com');
+expect(Employee.phone).toBe('135634');
+});
